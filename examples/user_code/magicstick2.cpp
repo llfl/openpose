f1a4eb724fcad0645b692fb273103c7afb226053
Kun <quinnHugh@hotmail.com>
2020-03-15 22:03:57 +0800

    Update magicstick2.cpp

--- examples/user_code/magicstick2.cpp
+++ examples/user_code/magicstick2.cpp
@@ -70,9 +70,12 @@ public:
                         if (fin_state == 0 && stick_point.size() > 1)
                         {
                             cv::Mat stick_pattern(cvOutputData.rows, cvOutputData.cols, CV_8UC1, 255);
-                            for (int i = 0; i<stick_point.size(); ++i )
+                            for (int i = 1; i<stick_point.size(); ++i )
                             {
-                                stick_pattern.at<cv::Vec3b>(stick_point[i][0],stick_point[i][1]) = 0;
+                                // stick_pattern.at<cv::Vec3b>(stick_point[i][0],stick_point[i][1]) = 0;
+                                cv::Point a(stick_point[i-1][0],stick_point[i-1][1]);
+                                cv::Point b(stick_point[i][0],stick_point[i][1]);
+                                cv::line(stick_pattern, a, b, 0, 2);
                             }
                             pattern_no ++;
                             cv::imshow(std::to_string(pattern_no), stick_pattern);
